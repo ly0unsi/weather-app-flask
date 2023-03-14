7d254d6a927291f66955aa9750a436380c36b420
ly0unsi <mcabdo25@gmail.com>
2023-03-14 20:56:30 +0100

    commit

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from flask import Flask
-app = Flask(__name__)
+myapp = Flask(__name__)
 if __name__ == "__main__":
-    app.config.setdefault('WTF_CSRF_METHODS', ['PUT'])
+    myapp.config.setdefault('WTF_CSRF_METHODS', ['PUT'])
 
-    myapp = app.run(debug=True)
+    myapp.run(debug=True)
