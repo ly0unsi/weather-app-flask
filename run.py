5883f96040e621f0e5506ea11a9ad0c8832cab69
ly0unsi <mcabdo25@gmail.com>
2023-03-14 22:00:47 +0100

    commit

--- run.py
+++ run.py
@@ -1,6 +1,2 @@
-from flask import Flask
-myapp = Flask(__name__)
-if __name__ == "__main__":
-    myapp.config.setdefault('WTF_CSRF_METHODS', ['PUT'])
-
-    myapp.run(debug=True)
+from weatherApp.app import app
+app = app
